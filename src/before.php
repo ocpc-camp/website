afb7a74dd767ee0b4bcb71124300cb2e583fa7f6
thuustalu <thuustalu@gmail.com>
2023-01-03 06:24:01 +0200

    move site to src

---
+++ src/before.php
@@ -0,0 +1,35 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Osijek Competitive Programming Camp</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css" />
+    <link rel="stylesheet" href="site.css" />
+  </head>
+  <body>
+    <div class="container body-content">
+        <nav class="navbar navbar-expand-lg">
+          <div class="container-fluid">
+            <a class="navbar-brand" href="index.php">Osijek Competitive Programming Camp</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+              <ul class="navbar-nav">
+                <li class="nav-item">
+                  <a class="nav-link" href="details.php">Details</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="schedule.php">Schedule</a>
+                </li>
+              </ul>
+              <ul class="navbar-nav ms-auto">
+                <li class="nav-item">
+                  <a class="nav-link" href="contact.php">Contact</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+    
