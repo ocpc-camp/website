be01904fcd68d1331512e42f92923657e3727e86
thuustalu <thuustalu@gmail.com>
2023-01-04 13:02:51 +0200

    think-cell link change

--- src/after.php
+++ src/after.php
@@ -5,7 +5,7 @@
     <div class="col">
       Sponsors
       <a href="https://www.janestreet.com/join-jane-street/open-roles/?type=students-and-new-grads&location=london"><img src="/images/Jane Street.svg" class="img-fluid osi-partner-logo" width="150"/></a>
-      <a href="https://think-cell.com/en/career"><img src="/images/think-cell.svg" class="img-fluid osi-partner-logo" width="150"/></a>          
+      <a href="https://think-cell.com/developers"><img src="/images/think-cell.svg" class="img-fluid osi-partner-logo" width="150"/></a>          
       <a href="https://pinely.com/"><img src="/images/Pinely.svg" class="img-fluid osi-partner-logo" /></a>
     </div>
     <div class="col-md-auto">
--- src/sponsors.php
+++ src/sponsors.php
@@ -3,52 +3,52 @@
 <h2>Our Sponsors</h2>
 
 <h4>
-<a href="https://www.janestreet.com/join-jane-street/open-roles/?type=students-and-new-grads&location=london">
-    <img src="/images/Jane Street.svg" width="150px">
-</a>
+  <a href="https://www.janestreet.com/join-jane-street/open-roles/?type=students-and-new-grads&location=london">
+    <img src="images/Jane Street.svg" width="150px">
+  </a>
 </h4>
 <p>
-Jane Street is a quantitative trading firm with offices worldwide. We hire smart, humble people who love to solve problems, build systems, and test theories. You'll learn something new every day in our office—whether it's connecting with a colleague to share perspectives, or participating in a talk, class, or game night. Our success is driven by our people and we never stop improving.
+  Jane Street is a quantitative trading firm with offices worldwide. We hire smart, humble people who love to solve problems, build systems, and test theories. You'll learn something new every day in our office—whether it's connecting with a colleague to share perspectives, or participating in a talk, class, or game night. Our success is driven by our people and we never stop improving.
 </p>
 
 <br>
 
 <h4>
-<a href="https://pinely.com/">
-    <img src="/images/Pinely.svg">
-</a>
+  <a href="https://pinely.com/">
+    <img src="images/Pinely.svg">
+  </a>
 </h4>
 <p>
-Pinely is a privately owned & funded algorithmic trading firm that is growing by leaps and bounds. We are based in Singapore, the Netherlands and Cyprus and our main focus is set on high frequency and ultra low latency trading.
+  Pinely is a privately owned & funded algorithmic trading firm that is growing by leaps and bounds. We are based in Singapore, the Netherlands and Cyprus and our main focus is set on high frequency and ultra low latency trading.
 </p>
 <p>
-We work in synergy to create a leading-edge & robust technology with the sole aim of exceling and establishing ourselves amongst the top trading companies around the globe. Our core team constantly faces various challenges such as developing strategies for trading, optimizing trading systems to achieve the lowest latency reactions to various market events, saving and processing large volumes of historical data.
+  We work in synergy to create a leading-edge & robust technology with the sole aim of exceling and establishing ourselves amongst the top trading companies around the globe. Our core team constantly faces various challenges such as developing strategies for trading, optimizing trading systems to achieve the lowest latency reactions to various market events, saving and processing large volumes of historical data.
 </p>
 <p>
-We are a team of mathematicians, programmers, engineers and computer scientists, driven by an immense passion for knowledge. The work we do requires a high ability to create effective C++ code, algorithmic thinking and mathematical intuition, which attracts winners, awardees and medalists of various competitions in their respective fields such as ICPC, IMC, HITB PRO CTF and Google HashCode etc.
+  We are a team of mathematicians, programmers, engineers and computer scientists, driven by an immense passion for knowledge. The work we do requires a high ability to create effective C++ code, algorithmic thinking and mathematical intuition, which attracts winners, awardees and medalists of various competitions in their respective fields such as ICPC, IMC, HITB PRO CTF and Google HashCode etc.
 </p>
 <p>
-At Pinely we have a flat organizational structure with a collegial environment and an idea-driven culture to stay ahead of the curve.
+  At Pinely we have a flat organizational structure with a collegial environment and an idea-driven culture to stay ahead of the curve.
 </p>
 <p>
-Find out more about us on <a href="http://pinely.com">our website</a>.
+  Find out more about us on <a href="http://pinely.com">our website</a>.
 </p>
 <p>
-If you want to join our team please send your CV on <a href="mailto:hr@pinely.com">hr@pinely.com</a>.
+  If you want to join our team please send your CV on <a href="mailto:hr@pinely.com">hr@pinely.com</a>.
 </p>
 
 <h4>
-<a href="https://think-cell.com/en/career">
-    <img src="/images/think-cell.svg" width="250px">
-</a>
+  <a href="https://think-cell.com/developers">
+    <img src="images/think-cell.svg" width="250px">
+  </a>
 </h4>
 <p>
-think-cell is a fast-paced software company in Berlin, Germany, with a focus on developing graphics products that stand out from the crowd. More than 1,000,000 users world-wide rely on our software for their daily business as it makes creating graphical presentations so much easier, faster and more enjoyable. Among our customers are many renowned consulting companies and large international corporations.
+  think-cell is a fast-paced software company in Berlin, Germany, with a focus on developing graphics products that stand out from the crowd. More than 1,000,000 users world-wide rely on our software for their daily business as it makes creating graphical presentations so much easier, faster and more enjoyable. Among our customers are many renowned consulting companies and large international corporations.
 </p>
 
 <h4>Sponsor us</h4>
 <p>
-Interested in joining this list? Contact us at <a href="mailto:ocpc@mathos.hr">ocpc@mathos.hr</a>!
+  Interested in joining this list? Contact us at <a href="mailto:ocpc@mathos.hr">ocpc@mathos.hr</a>!
 </p>
 
 <?php include 'after.php'; ?>
