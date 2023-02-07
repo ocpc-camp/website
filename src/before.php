fce0b7436eb02ed90b25046ca9a443466d692d18
thuustalu <thuustalu@gmail.com>
2023-02-08 00:10:36 +0100

    site logo

--- src/before.php
+++ src/before.php
@@ -5,13 +5,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Osijek Competitive Programming Camp</title>
     <link rel="stylesheet" href="/css/bootstrap.min.css" />
-    <link rel="stylesheet" href="/site.css?v=2" />
+    <link rel="stylesheet" href="/site.css?v=3" />
   </head>
   <body>
     <div class="container body-content">
         <nav class="navbar navbar-expand-lg">
           <div class="container-fluid">
-            <a class="navbar-brand" href="/index.php">Osijek Competitive Programming Camp</a>
+            <a class="navbar-brand" href="/index.php"><img class="site-logo" src="/images/ocpc.svg" alt="ocpc logo"></a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
               <span class="navbar-toggler-icon"></span>
             </button>
