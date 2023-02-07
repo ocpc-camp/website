559a92946633426452506b0bc64769f37232a198
thuustalu <thuustalu@gmail.com>
2023-02-08 00:16:02 +0100

    tweak logo size

--- src/before.php
+++ src/before.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Osijek Competitive Programming Camp</title>
     <link rel="stylesheet" href="/css/bootstrap.min.css" />
-    <link rel="stylesheet" href="/site.css?v=3" />
+    <link rel="stylesheet" href="/site.css?v=4" />
   </head>
   <body>
     <div class="container body-content">
