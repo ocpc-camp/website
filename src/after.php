5b980c79e01063dff7af51a790671a49f3483298
thuustalu <thuustalu@gmail.com>
2023-09-05 19:31:35 +0200

    ICPC logo

--- src/after.php
+++ src/after.php
@@ -7,6 +7,12 @@
       <a href="https://www.janestreet.com/join-jane-street/open-roles/?type=students-and-new-grads&location=london"><img src="/images/Jane Street.svg" class="img-fluid osi-partner-logo" width="150"/></a>
       <a href="https://pinely.com/"><img src="/images/Pinely.svg" class="img-fluid osi-partner-logo" /></a>
     </div>
+    <div class="col">
+      Partners
+      <a href="https://icpc.foundation/">
+        <img src="/images/ICPC.svg" class="img-fluid osi-partner-logo "/>
+      </a>
+    </div>
     <div class="col-md-auto">
       Host
       <a href="https://www.mathos.unios.hr/index.php/en/">
