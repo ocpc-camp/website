af5810571116c813e0d3b82838d29a741ee112f1
thuustalu <thuustalu@gmail.com>
2023-09-17 02:02:50 +0200

    add scoreboard and contest link day 2

--- src/contest-info.php
+++ src/contest-info.php
@@ -13,6 +13,11 @@
     <td><a href="https://osijek2023s-d1.eolymp.io">login</a></td>
     <td><a href="https://osijek2023s.eolymp.space/s/d1">scoreboard</a></td>
   </tr>
+  <tr>
+    <td><b>Day 2: Zagreb Contest</b></td>
+    <td><a href="https://osijek2023s-d1.eolymp.io">login</a></td>
+    <td><a href="https://osijek2023s.eolymp.space/s/d2">scoreboard</a></td>
+  </tr>
 </table>
 
 <h3>Camp materials and silence period</h3>
