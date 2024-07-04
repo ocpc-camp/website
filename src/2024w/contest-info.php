9388568ccd61c8cf63d99a6a0ebef52e8a0096d6
thuustalu <thuustalu@gmail.com>
2024-07-04 12:35:26 +0300

    restore 2024w contest info

---
+++ src/2024w/contest-info.php
@@ -0,0 +1,150 @@
+<?php include 'before.php'; ?>
+
+<h2>Contest info</h2>
+
+<table class="table">
+  <tr>
+    <td><b>Test contest</b></td>
+    <td><a href="https://osijek2024w.eolymp.space/contests/kp04o8i8fh0p5ecta9theqjaj8">login</a></td>
+    <td></td>
+  </tr>
+  <tr>
+    <td><b>Day 1: Anton Trygub Contest 2</b></td>
+    <td><a href="https://osijek2024w.eolymp.space/contests/1p98bkcd2951l6f4kecm412g6g">login</a></td>
+    <td><a href="https://osijek2024w.eolymp.space/s/d1">scoreboard</a></td>
+  </tr>
+  <tr>
+    <td><b>Day 2: Portuguese Contest</b></td>
+    <td><a href="https://osijek2024w.eolymp.space/contests/sri1d20gl95kb9f8g44haog8d8">login</a></td>
+    <td><a href="https://osijek2024w.eolymp.space/s/d2">scoreboard</a></td>
+  </tr>
+  <tr>
+    <td><b>Day 4: Latvian Contest</b></td>
+    <td><a href="https://osijek2024w.eolymp.space/contests/b3amafs3i56fbe5lc9m485h420">login</a></td>
+    <td><a href="https://osijek2024w.eolymp.space/s/d4">scoreboard</a></td>
+  </tr>
+  <tr>
+    <td><b>Day 5: Battr Contest</b></td>
+    <td><a href="https://osijek2024w.eolymp.space/contests/f71cbkpdp94vl09m8o8ao3aa28">login</a></td>
+    <td><a href="https://osijek2024w.eolymp.space/s/d5">scoreboard</a></td>
+  </tr>
+  <tr>
+    <td><b>Day 6: Potyczki Algorytmiczne Contest</b></td>
+    <td><a href="https://osijek2024w.eolymp.space/contests/21rbg5mfap37185o9so4im4oog">login</a></td>
+    <td><a href="https://osijek2024w.eolymp.space/s/d6">scoreboard</a></td>
+  </tr>
+  <tr>
+    <td><b>Day 8: Borys Minaiev Contest</b></td>
+    <td><a href="https://osijek2024w.eolymp.space/contests/tjlkpng86d1q360pq64qh2boj0">login</a></td>
+    <td><a href="https://osijek2024w.eolymp.space/s/d8">scoreboard</a></td>
+  </tr>
+  <tr>
+    <td><b>Day 9: Potluck Contest</b></td>
+    <td><a href="https://osijek2024w.eolymp.space/contests/p4dvu5bhep32d4r0lo1ppns8fo">login</a></td>
+    <td><a href="https://osijek2024w.eolymp.space/s/d9">scoreboard</a></td>
+  </tr>
+  <tr>
+    <td><b>All contests</b></td>
+    <td><a href="https://osijek2024w.eolymp.space/en/s/all">solve counts</a></td>
+    <td><a href="http://acmallukrainian.ho.ua/camps/osijek/2024/winter/combined/standings.html">ratings</a></td>
+  </tr>
+</table>
+
+<h3>Camp materials and silence period</h3>
+<p>
+  Based on your feedback, we will have a silence period until the end of
+  World Finals 2023 (which has been postponed to somewhere during 2024),
+  during which camp materials will not be released to the public.
+  Therefore, we ask participants to not discuss the problems in public until that date.
+</p>
+<p>
+  After the camp ends, we will give all participants access to camp materials.
+</p>
+<p>
+  Most of our contests are fresh and developed for this camp. A small number of contests
+  may be based on previous contests that have not been released to the general public.
+  If you have seen some problems of a contest before, you can't participate on that day
+  (and your participation fee will be reduced accordingly). We will privately contact participants
+  who might be affected.
+</p>
+
+<h3>Rules</h3>
+<p>
+Contests will mostly follow
+<a href="https://www.win.tue.nl/~wstomv/contests/acm-icpc-rules-summary.html">classical ICPC rules</a>,
+in particular:
+</p>
+<ul>
+  <li>Each contest typically lasts 5 hours.</li>
+  <li>Ranking of teams is based on:
+    <ul>
+      <li>Number of problems solved;</li>
+      <li>In case of a tie, total time for all accepted problems;</li>
+      <li>Each rejected submission for an accepted problem adds 20 minutes penalty time;</li>
+      <li>If there is still a tie, the time of acceptance for the last problem;</li>
+    </ul>
+  </li>
+  <li>It is possible to ask for clarifications about the problem in case of possible ambiguity
+    or error. The answers will typically be restricted to "Yes", "No" or "No comments";</li>
+  <li>The scoreboard is public and updated during the contest, except for the last hour, during
+    which it will be frozen.</li>
+</ul>
+<p>
+  We follow the "1pc" rule: in a team, <b>only one person is allowed to use a computer at any
+    given point in time.</b>
+</p>
+<ul>
+  <li>For onsite participants, this means that a team will only have access to one computer
+    during a contest.</li>
+  <li>Online participants may use different computers (e.g. when the members of a team are not
+    participating in the same room). We rely on your honesty to keep track and make sure that
+    only one person is using a computer at any given time.</li>
+</ul>
+<p>
+  Using the Internet during a contest is not forbidden. <b>However, if your team is preparing for
+    ICPC or a similar contest, it is heavily discouraged; we recommend using an ICPC-compliant Team
+    Reference Document instead.</b> Communicating with other people or large language models (e.g.
+  talking to other people about the problems, asking questions on stackoverflow or similar forums,
+  prompting ChatGPT) is forbidden.
+</p>
+<p>
+  Terms and conditions of <a href="legal.php">the camp</a> and
+  <a href="https://support.eolymp.com/policies/terms">the contest system</a> apply.
+</p>
+
+<h3>Contest environment</h3>
+
+<p>
+  Our contests will be hosted on <a href="https://www.eolymp.com/en/">eolymp</a>. More detailed
+  information about the system, such as compilation/execution lines and example solutions can be
+  found <a href="https://www.eolymp.com/en/help/">here</a>. Participants will receive
+  credentials by email.
+</p>
+
+<h3>Onsite rules</h3>
+<p>
+  Onsite teams will be given access to computers in the host university. You will be provided a
+  VirtualBox virtual machine running Xubuntu 22.04. Within the virtual machine you have sudo
+  permissions and you are allowed to do anything you want to it. Several common editors, IDEs
+  and utilities have been pre-installed:
+  <ul>
+    <li> Editors: vim, gvim, nvim, emacs, nano, geany, kate, gedit </li>
+    <li> IDEs: IntelliJ IDEA Community, PyCharm Community, Visual Studio Code </li>
+    <li> Compilers: g++, pypy3 </li>
+    <li> gdb </li>
+  </ul>
+  You may also use the host operating system (Windows 10 or 11) if you prefer, but we can't guarantee
+  the availability of any particular program.
+</p>
+
+<p>
+  We allow onsite teams to use their own computer instead. In particular, if you rely on a very
+  particular workflow (e.g. a specific IDE, many customizations), we encourage you to bring your own
+  computer. Keep in mind though that the rule about using only one computer still applies.
+</p>
+
+<p>
+  Onsite teams will have access to a printer.
+</p>
+
+<?php include 'after.php'; ?>
