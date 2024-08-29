6620cb20416c722bb3e08db914b9c929edc2aa83
thuustalu <thuustalu@gmail.com>
2024-08-29 21:15:58 +0300

    2024s schedule

--- src/schedule.php
+++ src/schedule.php
@@ -3,11 +3,89 @@
 <h2>Schedule</h2>
 
 <p>
-  The camp consists of 7 contest days and 2 days off inbetween.
-  A typical day will consist of a 5-hour practice contest starting
-  at 10:00 CEST, and a tutorial/analysis session in the afternoon.
-  The days off will likely be Monday and Friday.
-  A precise schedule will be announced later.
+  The camp consists of 7 contest days and
+  2 days off inbetween.
 </p>
+
+<p>
+  All times are in CEST (UTC +2). Online participants have the opportunity to
+  start the contest in a 24h window, starting anytime after the start of the
+  original contest.
+</p>
+
+<div class="row">
+  <div class="col-lg-4 schedule-cell">
+    <h6>Day 1, August 31</h6>
+    Kharkiv Contest <br>
+    <ul>
+      <li><b>09:30 - 10:00</b> Registration</li>
+      <li><b>10:00 - 10:30</b> Opening notes</li>
+      <li><b>10:30 - 12:30</b> Practice round & lunch</li>
+      <li><b>12:30 - 17:30</b> Contest</li>
+      <li><b>18:30 - 20:00</b> Analysis & discussion</li>
+    </ul>
+  </div>
+  <div class="col-lg-4 schedule-cell">
+    <h6>Day 2, September 1</h6>
+    Jagiellonian Contest <br>
+    <ul>
+      <li><b>10:00 - 15:00</b> Contest</li>
+      <li><b>16:30 - 18:00</b> Analysis & discussion</li>
+    </ul>
+  </div>
+  <div class="col-lg-4 schedule-cell">
+    <h6>Day 3, September 2</h6>
+    Day off. Activities TBD.
+  </div>
+</div>
+<div class="row">
+  <div class="col-lg-4 schedule-cell">
+    <h6>Day 4, September 3</h6>
+    wuhudsm Contest <br>
+    <ul>
+      <li><b>10:00 - 15:00</b> Contest</li>
+      <li><b>16:30 - 18:00</b> Analysis & discussion</li>
+    </ul>
+  </div>
+  <div class="col-lg-4 schedule-cell">
+    <h6>Day 5, September 4</h6>
+    Potluck Contest <br>
+    <ul>
+      <li><b>10:00 - 15:00</b> Contest</li>
+      <li><b>16:30 - 18:00</b> Analysis & discussion</li>
+    </ul>
+  </div>
+  <div class="col-lg-4 schedule-cell">
+    <h6>Day 6, September 5</h6>
+    Chinese NOI Contest <br>
+    <ul>
+      <li><b>10:00 - 15:00</b> Contest</li>
+      <li><b>16:30 - 18:00</b> Analysis & discussion</li>
+    </ul>
+  </div>
+</div>
+<div class="row">
+  <div class="col-lg-4 schedule-cell">
+    <h6>Day 7, September 6</h6>
+    Day off. Activities TBD.
+  </div>
+  <div class="col-lg-4 schedule-cell">
+    <h6>Day 8, September 7</h6>
+    Xi'an Contest <br>
+    <ul>
+      <li><b>10:00 - 15:00</b> Contest</li>
+      <li><b>16:30 - 18:00</b> Analysis & discussion</li>
+    </ul>
+  </div>
+  <div class="col-lg-4 schedule-cell">
+    <h6>Day 9, September 8</h6>
+    Farhod Contest <br>
+    <ul>
+      <li><b>10:00 - 15:00</b> Contest</li>
+      <li><b>16:30 - 18:00</b> Analysis & discussion</li>
+      <li><b>20:00? - </b> Closing dinner</li>
+    </ul>
+  </div>  
+</div>
 
 <?php include 'after.php'; ?>
